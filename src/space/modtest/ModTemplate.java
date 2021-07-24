71197a81f1f52d8fa54b13868bf0554df6ee7ebf
spacecr4zygamer <lucazach04@googlemail.com>
2021-07-24 22:28:31 +0200

    Testing ig
    i need desperately help

--- src/space/modtest/ModTemplate.java
+++ src/space/modtest/ModTemplate.java
@@ -1,14 +1,21 @@
-package sonnicon.modtemplate;
+package space.modtest;
 
+import arc.util.Log;
 import mindustry.mod.Mod;
 
 public class ModTemplate extends Mod{
 
+    public ModTemplate() {
+        Log.info("I need help otherwise?");
+    }
+
     @Override
     public void init(){
+        Log.info("I need help");
     }
 
     @Override
     public void loadContent(){
+
     }
 }
