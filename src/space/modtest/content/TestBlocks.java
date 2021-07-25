13d4f967e15be10ca8095b1fc98df7a7c127c204
spacecr4zygamer <lucazach04@googlemail.com>
2021-07-25 14:33:27 +0200

    Extended loadcontent

--- src/space/modtest/content/TestBlocks.java
+++ src/space/modtest/content/TestBlocks.java
@@ -1,18 +1,29 @@
 package space.modtest.content;
 
+import mindustry.content.Items;
 import mindustry.ctype.ContentList;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import space.modtest.world.blocks.power.SingleLiquidGenerator;
+
+import static mindustry.type.ItemStack.with;
 
 public class TestBlocks implements ContentList {
 
     // power
-    public static Block SingleLiquidGenerator;
+    public static Block steamengine;
 
     @Override
     public void load() {
-        SingleLiquidGenerator = new SingleLiquidGenerator("SingleLiquidGenerator"){{
-            
+        steamengine = new SingleLiquidGenerator("Steam Engine"){{
+            health = 150;
+            requirements(Category.power,with(Items.lead,15,Items.silicon,10));
+            outputsPower = true;
+            consumesPower = false;
+            hasPower = true;
+            size = 2;
+            powerProduction = 10;
         }};
     }
 }
