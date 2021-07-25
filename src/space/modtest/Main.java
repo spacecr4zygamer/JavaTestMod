59d5b9c6738805f8d1d4667a1c3f60a15af64cb8
spacecr4zygamer <lucazach04@googlemail.com>
2021-07-25 02:42:33 +0200

    SingleLiquidGenerator
    Adding SingleLiquidGenerator Class?

--- src/space/modtest/Main.java
+++ src/space/modtest/Main.java
@@ -2,14 +2,17 @@ package space.modtest;
 
 import arc.util.Log;
 import mindustry.mod.Mod;
+import mindustry.world.blocks.power.BurnerGenerator;
+import mindustry.world.blocks.power.ItemLiquidGenerator;
+import mindustry.world.blocks.power.PowerGenerator;
 
-public class ModTemplate extends Mod{
 
-    public ModTemplate() {
+public class Main extends Mod{
+
+    public Main() {
         Log.info("I need help otherwise?");
         //I need help D:
     }
-
     @Override
     public void init(){
         Log.info("I need help");
---
+++ src/space/modtest/SingleLiquidGenerator.java
@@ -0,0 +1,24 @@
+package space.modtest;
+
+import mindustry.type.Item;
+import mindustry.type.Liquid;
+import mindustry.world.blocks.power.ItemLiquidGenerator;
+
+public class SingleLiquidGenerator extends ItemLiquidGenerator {
+
+    public SingleLiquidGenerator(String name) {
+        super(name);
+        defaults = true;
+    }
+
+    @Override
+    protected float getItemEfficiency(Item item){
+        return 0f;
+    }
+
+    @Override
+    protected float getLiquidEfficiency(Liquid liquid){
+        return 1f;
+    }
+
+}
